c58fc22ffedb5db9e45b6b85b34e21ec7f79102b
DiiSollertia <xz.khoo.work@gmail.com>
2023-02-15 13:45:39 +0800

    Smoother GUI experience

--- src/FHXparser/phasediff/gui.py
+++ src/FHXparser/phasediff/gui.py
@@ -5,8 +5,8 @@ import os.path
 def selectfiles():
     layout = [
         [sg.Text('Select 2 FHX files to compare.')],
-        [sg.Text('File 1'), sg.Input(size=(60,1)), sg.FileBrowse()],
-        [sg.Text('File 2'), sg.Input(size=(60,1)), sg.FileBrowse()],
+        [sg.Text('File 1'), sg.Input(size=(60,1)), sg.FileBrowse(file_types=[('DeltaV Config Files','*.fhx')])],
+        [sg.Text('File 2'), sg.Input(size=(60,1)), sg.FileBrowse(file_types=[('DeltaV Config Files','*.fhx')])],
         [sg.Submit('Strip FHX'), sg.Submit('Simple Compare'), sg.Cancel()],
         ]
     window = sg.Window('Phase Compare', layout)
@@ -16,6 +16,7 @@ def selectfiles():
             window.close()
             return event, None, None
         elif values[0] and values[1]:
+            window.close()
             return event, values[0], values[1]
 
 def showhtml(filename):
