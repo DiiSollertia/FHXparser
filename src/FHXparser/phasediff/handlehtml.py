5caff8cb40f8af03fc9d480522e8819297b2c981
DiiSollertia <xz.khoo.work@gmail.com>
2023-02-15 13:29:11 +0800

    Improved file loading before diff

--- src/FHXparser/phasediff/handlehtml.py
+++ src/FHXparser/phasediff/handlehtml.py
@@ -1,11 +1,11 @@
-import difflib
+import difflib as dl
 
 def makehtml(file1, file2):
-    with open(file1) as ff:
-        fromlines = ff.readlines()
-    with open(file2) as tf:
-        tolines = tf.readlines()
-    return difflib.HtmlDiff().make_file(fromlines,tolines,file1,file2,context=True,numlines=1) 
+    with open(file1,  'r', encoding='utf-16') as ff:
+        fromlines = [l.strip() for l in ff if l.strip()]
+    with open(file2,  'r', encoding='utf-16') as tf:
+        tolines = [l.strip() for l in tf if l.strip()]
+    return dl.HtmlDiff().make_file(fromlines,tolines,file1,file2,context=True,numlines=0)
 
 def writehtml(htmlstring, location):
     output = location + '/difftable.html'
